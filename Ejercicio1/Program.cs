b9b06e9ae7db375714f695492d7473c18d91ddf2
claudiomontoya <claudio.montoya@biopc.cl>
2021-09-01 21:49:35 -0400

    primer commit ejercicio 1

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ingresar datos de 3 personas 
+            // Calculo del IMC 
+            // Calculo tasa metabolica basal TMB  ecuacion de Harris-benedict 
+
+            //TMB x 1,2: Poco o ningún ejercicio
+            //TMB x 1,375: Ejercicio ligero (1 a 3 días a la semana)
+            //TMB x 1,55: Ejercicio moderado (3 a 5 días a la semana)
+            //TMB x 1,72: Deportista(6 - 7 días a la semana)
+            //TMB x 1,9: Atleta(Entrenamientos mañana y tarde)
+
+            //Se requiere buscar la formula para poder disminuir o aumentar de peso
+            //segun lo que requiera el usuario en un rango de una semana, la variacion de la disminucion o aumento
+            //es de maximo 1kilo
+
+
+        }
+    }
+}
